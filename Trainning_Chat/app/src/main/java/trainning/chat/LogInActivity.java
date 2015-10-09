32c15662d601c5a7bc642b33dea2706d0317d2c0
nguyen5891 <nguyenxd5891@gmail.com>
2015-10-09 16:14:32 +0700

    edit login and register

--- Trainning_Chat/app/src/main/java/trainning/chat/LogInActivity.java
+++ Trainning_Chat/app/src/main/java/trainning/chat/LogInActivity.java
@@ -1,47 +1,134 @@
 package trainning.chat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by ASUS on 09/10/2015.
  */
-public class LogInActivity extends Activity {
+public class LogInActivity extends AppCompatActivity {
     private EditText mEdtEmail, mEdtpass;
     private Button mBtnLogIn;
     private User mUser;
     private DatabaseHandler mDatabaseHandler;
+    private Toolbar toolbar;
+    private boolean loginID_isLegal = true;
+    private boolean loginPW_isLegal = true;
+    public final int USERNAME_MAX = 256;
+    public final int USERNAME_MIN = 3;
+    public final int PASSWORD_MIN = 8;
+    public Pattern usernameLegalPattern = Pattern.compile("^[a-zA-Z0-9_.-@]+$");
+    public Pattern emailLegalPattern = Pattern
+            .compile("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" + "\\@"
+                    + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\."
+                    + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         mEdtEmail = (EditText) findViewById(R.id.etEmail);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Welcome");
+        setSupportActionBar(toolbar);
+        mDatabaseHandler = new DatabaseHandler(this);
         mEdtpass = (EditText) findViewById(R.id.etPassword);
+        mEdtEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mEdtEmail.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!emailLegalPattern.matcher(editable).matches()) {
+                    loginID_isLegal = false;
+                } else {
+                    loginID_isLegal = true;
+                }
+            }
+        });
+
+        mEdtpass.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                mEdtpass.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (mEdtpass.length() < PASSWORD_MIN) {
+                    // mLoginPassword.setError("< "+PASSWORD_MIN);
+                    loginPW_isLegal = false;
+                } else {
+                    // mLoginPassword.setError(null);
+                    loginPW_isLegal = true;
+                }
+            }
+
+        });
+
         mBtnLogIn = (Button) findViewById(R.id.btnLogIn);
         mBtnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = mEdtEmail.getText().toString();
                 String password = mEdtpass.getText().toString();
-                mDatabaseHandler = new DatabaseHandler(getApplicationContext());
-                mUser = mDatabaseHandler.getUser(email);
-                if (!email.equals("") && !password.equals("")) {
-                    if (email.equals(mUser.getEmail()) && password.equals(mUser.getPassWord())) {
-                        Toast.makeText(getApplicationContext(), "LOGIN SUCCESS", Toast.LENGTH_SHORT).show();
 
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Email or Password not match", Toast.LENGTH_SHORT).show();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Can not be blank", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                if (loginID_isLegal && loginPW_isLegal) {
+                    mUser = mDatabaseHandler.getUser(email);
+                    if (email.equals(mUser.getEmail()) && password.equals(mUser.getPassWord())) {
+                        startActivity(new Intent(getApplicationContext(), ChatActivity.class));
                     }
 
 
                 } else {
-                    Toast.makeText(getApplicationContext(), "Email or password is not empty", Toast.LENGTH_SHORT).show();
+                    if (!loginID_isLegal) {
+                        if (mEdtEmail.length() > USERNAME_MAX) {
+                            mEdtEmail.setError(">" + USERNAME_MAX);
+                        } else if (mEdtEmail.length() < USERNAME_MIN) {
+                            mEdtEmail.setError("<" + USERNAME_MIN);
+
+                        } else {
+                            mEdtEmail.setError("Email not match");
+                        }
+                    }
+                    if (!loginPW_isLegal) {
+                        if (mEdtpass.length() < PASSWORD_MIN) {
+                            mEdtpass.setError("<" + PASSWORD_MIN);
+                        }
+                    }
                 }
+
             }
         });
     }
--- Trainning_Chat/app/src/main/java/trainning/chat/RegisterActivity.java
+++ Trainning_Chat/app/src/main/java/trainning/chat/RegisterActivity.java
@@ -3,52 +3,144 @@ package trainning.chat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by ASUS on 09/10/2015.
  */
-public class RegisterActivity extends Activity {
+public class RegisterActivity extends AppCompatActivity {
     private EditText edtUsername, edtEmail, edtPassword, edtConfirmPass;
     private Button btnRegister;
+    private Toolbar toolbar;
+    private boolean loginID_isLegal = true;
+    private boolean loginPW_isLegal = true;
+    public final int USERNAME_MAX = 256;
+    public final int USERNAME_MIN = 3;
+    public final int PASSWORD_MIN = 8;
+    public Pattern usernameLegalPattern = Pattern.compile("^[a-zA-Z0-9_.-@]+$");
+    public Pattern emailLegalPattern = Pattern
+            .compile("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" + "\\@"
+                    + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\."
+                    + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Create Account");
+        setSupportActionBar(toolbar);
         edtUsername = (EditText) findViewById(R.id.etUserName);
         edtEmail = (EditText) findViewById(R.id.etEmail);
         edtConfirmPass = (EditText) findViewById(R.id.etConfirmPassword);
         edtPassword = (EditText) findViewById(R.id.etPassword);
         btnRegister = (Button) findViewById(R.id.btnReg);
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+
+        edtEmail.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                String userName = edtUsername.getText().toString();
-                String email = edtEmail.getText().toString();
-                String password = edtPassword.getText().toString();
-                String confirmPass = edtConfirmPass.getText().toString();
-                if (!email.equals("") && !password.equals("")) {
-                    if (password.equals(confirmPass)) {
-                        DatabaseHandler db = new DatabaseHandler(getApplicationContext());
-                        db.addUser(new User(userName, email, password));
-                        startActivity(new Intent(getApplicationContext(), LogInActivity.class));
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                edtEmail.setError(null);
+            }
 
-                    } else {
-                        Toast.makeText(getApplicationContext(), "password not match ", Toast.LENGTH_SHORT).show();
-
-                    }
-
-                } else {
-                    Toast.makeText(getApplicationContext(), "Email or password must not empty", Toast.LENGTH_SHORT).show();
-                }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!emailLegalPattern.matcher(editable).matches()) {
+                    loginID_isLegal = false;
+                } else {
+                    loginID_isLegal = true;
+                }
+            }
         });
+        edtPassword.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                edtPassword.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (edtPassword.length() < PASSWORD_MIN) {
+                    // mLoginPassword.setError("< "+PASSWORD_MIN);
+                    loginPW_isLegal = false;
+                } else {
+                    // mLoginPassword.setError(null);
+                    loginPW_isLegal = true;
+                }
+            }
+
+        });
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View view) {
+                                               String userName = edtUsername.getText().toString();
+                                               String email = edtEmail.getText().toString();
+                                               String password = edtPassword.getText().toString();
+                                               String confirmPass = edtConfirmPass.getText().toString();
+                                               if (email.isEmpty() || password.isEmpty()) {
+                                                   Toast.makeText(getApplicationContext(), "Can not be blank", Toast.LENGTH_SHORT).show();
+                                                   return;
+
+                                               }
+                                               if (loginID_isLegal && loginPW_isLegal) {
+                                                   if (password.equals(confirmPass)) {
+                                                       DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+                                                       db.addUser(new User(userName, email, password));
+                                                       startActivity(new Intent(getApplicationContext(), LogInActivity.class));
+
+                                                   } else {
+                                                       Toast.makeText(getApplicationContext(), "password not match ", Toast.LENGTH_SHORT).show();
+
+                                                   }
+
+                                               } else
+
+                                               {
+                                                   if (!loginID_isLegal) {
+                                                       if (edtEmail.length() > USERNAME_MAX) {
+                                                           edtEmail.setError(">" + USERNAME_MAX);
+                                                       } else if (edtEmail.length() < USERNAME_MIN) {
+                                                           edtEmail.setError("<" + USERNAME_MIN);
+
+                                                       } else {
+                                                           edtEmail.setError("Email not match");
+                                                       }
+                                                   }
+                                                   if (!loginPW_isLegal) {
+                                                       if (edtPassword.length() < PASSWORD_MIN) {
+                                                           edtPassword.setError("<" + PASSWORD_MIN);
+                                                       }
+                                                   }
+                                               }
+
+
+                                           }
+                                       }
+
+        );
 
 
     }
